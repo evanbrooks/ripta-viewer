ffffea948d7b0be51287a63a65e89dada928c7a8
Evan Brooks <brooks.ev@gmail.com>
2017-10-09 11:33:04 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ function setup() {
     .add(po.compass().pan("none"));
 
     tilemap.add(po.image()
-      .url(po.url("http://{S}tile.stamen.com/toner-background/{Z}/{X}/{Y}.png")
+      .url(po.url("http://{S}tile.stamen.com/toner-background/{Z}/{X}/{Y}@2x.png")
       .hosts(["", "a.", "b.", "c.", "d."])));
 }
 
